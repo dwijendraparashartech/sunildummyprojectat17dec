96850f2631dddd6042254e2b7e180d2e02a03218
dwijendra <dwijendratech@gmail.com>
2021-12-17 22:08:49 +0530

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  get 'home/index'
+  devise_for :users
+  root "articles#index"
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "/articles", to: "articles#index"
+  get "/articles/:id", to: "articles#show"
+end
+
+
